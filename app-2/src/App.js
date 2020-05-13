2d59732aa33f9048a3fb87e2c4de10b8fe5677f3
MegSherman <megoftheshermans@gmail.com>
2020-05-13 15:04:43 -0600

    app 2 complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      friends: ['Calista', 'Arianne', 'Jeena', 'Alisha', 'Josh', 'Ben', 'Kayla', 'Levi']
+    }
+  }
+
+  render() {
+    let friendsDisplay = this.state.friends.map ((element, index) => {
+      return <h2 key={index}> {element} </h2>
+    })
+    return <div className="App"> {friendsDisplay} </div>
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
