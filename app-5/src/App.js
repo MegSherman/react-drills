f49540dfa46a787baba138b8b39cf83fd6043963
MegSherman <megoftheshermans@gmail.com>
2020-05-14 00:02:05 -0600

    serious problems with app 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Image from "/.birdofparadise.jpeg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class FlowerImage extends Component {
+  render () {
+    return (
+      <div>
+        <Image src="'./birdofparadise"/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default class Child extends Component {
+  render () {
+    return (
+      <img src={this.props.url}/>
+    )
+  }
+}
